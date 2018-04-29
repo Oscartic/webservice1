ae18a8f845690509ae0c81a6fbe0b84c473b873f
Oscar Ballesteros <oscartic@gmail.com>
2018-04-28 23:47:25 -0300

    Despliege en Heroku

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -7,10 +7,12 @@ class LoginsController < ApplicationController
     @uploaded_file = Login.image_capture(params[:image])
     @response = HTTParty.post("http://orbticweb.com/rest/verify_user/#{@email}", body: {image: @uploaded_file})
     if @response.code == 401
+
       flash[:alert] = "Petición no procesada"
+      LoginNotificationMailer.login_attempt_notification(@email).deliver
       redirect_to root_path
     end
     #send mail
-    LoginNotificationMailer.login_attempt_notification(@email).deliver
+    LoginNotificationMailer.success_login_notification(@email).deliver
   end
 end
--- app/mailers/login_notification_mailer.rb
+++ app/mailers/login_notification_mailer.rb
@@ -6,10 +6,22 @@ class LoginNotificationMailer < ApplicationMailer
     message_params = {
         :from    => ENV['email'],
         :to      => @mail,
-        :subject => 'Sample Mail using Mailgun API',
-        :text    => 'This mail is sent using Mailgun API via mailgun-ruby'
+        :subject => 'Autenticación fallida',
+        :text    => 'Se ha intentado ingresar a la plataforma ORBTICAPP y el resultado ha sido fallido.'
     }
     mg_client.send_message ENV['domain'], message_params
+  end
+
+  def success_login_notification(email)
+  @mail = email
+  mg_client = Mailgun::Client.new ENV['api_key']
+  message_params = {
+      :from    => ENV['email'],
+      :to      => @mail,
+      :subject => 'Autenticación exitosa',
+      :text    => 'Se ha ingresado a la plataforma ORBTICAPP de forma exitosa.'
+  }
+  mg_client.send_message ENV['domain'], message_params
   end
 
 end
